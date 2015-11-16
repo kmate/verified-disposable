3431497492929a0a0df38199736a1139b9d735db
Karácsony Máté <k_mate@inf.elte.hu>
2015-11-16 15:36:24 +0100

    Refactoring instruction adapter

--- src/main/java/com/github/kmate/verified/disposable/UsageVerifierInstructionAdapter.java
+++ src/main/java/com/github/kmate/verified/disposable/UsageVerifierInstructionAdapter.java
@@ -1,5 +1,15 @@
 package com.github.kmate.verified.disposable;
 
+import static org.objectweb.asm.Opcodes.ACC_BRIDGE;
+import static org.objectweb.asm.Opcodes.ACC_PRIVATE;
+import static org.objectweb.asm.Opcodes.ACC_STATIC;
+import static org.objectweb.asm.Opcodes.ACC_SYNTHETIC;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
 import org.objectweb.asm.ClassReader;
 import org.objectweb.asm.Opcodes;
 import org.objectweb.asm.Type;
@@ -11,17 +21,22 @@ final class UsageVerifierInstructionAdapter extends InstructionAdapter {
 	private static final String USAGE_VERIFIER_CLASS_NAME = UsageVerifier.class.getName().replace('.', '/');
 	private static final String VERIFIER_SIGNATURE = "(L" + ClassUtils.DISPOSABLE_CLASS_NAME + ";Ljava/lang/String;)V";
 
+	private static final Set<String> UNCHECKED_METHODS = Collections
+			.unmodifiableSet(new HashSet<String>(Arrays.asList("<init>", "<clinit>", "getClass", "isDisposed")));
+
+	private static final int HAS_NO_ACCESS = ACC_STATIC + ACC_PRIVATE + ACC_SYNTHETIC + ACC_BRIDGE;
+
 	private final int access;
-	private final String name;
+	private final String methodName;
 	private final ClassReader reader;
 	private final ClassLoader loader;
 	private final LocalVariablesSorter variables;
 
-	UsageVerifierInstructionAdapter(int access, String name, ClassReader reader, ClassLoader loader,
+	UsageVerifierInstructionAdapter(int access, String methodName, ClassReader reader, ClassLoader loader,
 			LocalVariablesSorter variables) {
 		super(Opcodes.ASM5, variables);
 		this.access = access;
-		this.name = name;
+		this.methodName = methodName;
 		this.reader = reader;
 		this.loader = loader;
 		this.variables = variables;
@@ -29,19 +44,33 @@ final class UsageVerifierInstructionAdapter extends InstructionAdapter {
 
 	@Override
 	public void visitCode() {
-		// FIXME: watch access modifiers
-		if (!"<init>".equals(name) && !"isDisposed".equals(name)
-				&& ClassUtils.isDisposableClass(reader.getClassName(), loader)) {
+		if (shouldInstrumentMethod()) {
 			visitVarInsn(Opcodes.ALOAD, 0);
-			aconst(name);
+			aconst(methodName);
 			invokestatic(USAGE_VERIFIER_CLASS_NAME, "verifyMethodInvocation", VERIFIER_SIGNATURE, false);
 		}
 		super.visitCode();
 	}
 
+	private boolean shouldInstrumentMethod() {
+		return hasPublicAccess() && !isUncheckedMethod() && ClassUtils.isDisposableClass(reader.getClassName(), loader);
+	}
+
+	private boolean hasPublicAccess() {
+		return 0 == (access & HAS_NO_ACCESS);
+	}
+
+	private boolean isUncheckedMethod() {
+		return isConstructor() || UNCHECKED_METHODS.contains(methodName);
+	}
+
+	private boolean isConstructor() {
+		return reader.getClassName().equals(methodName);
+	}
+
 	@Override
 	public void getfield(String owner, String name, String desc) {
-		if (isDisposableClass(owner)) {
+		if (isDisposableExternalClass(owner)) {
 			dup();
 			aconst(name);
 			invokestatic(USAGE_VERIFIER_CLASS_NAME, "verifyFieldRead", VERIFIER_SIGNATURE, false);
@@ -51,7 +80,7 @@ final class UsageVerifierInstructionAdapter extends InstructionAdapter {
 
 	@Override
 	public void putfield(String owner, String name, String desc) {
-		if (isDisposableClass(owner)) {
+		if (isDisposableExternalClass(owner)) {
 			Type fieldType = Type.getType(desc);
 			Type ownerType = Type.getType("L" + owner + ";");
 			int newValueLocal = variables.newLocal(fieldType);
@@ -67,7 +96,7 @@ final class UsageVerifierInstructionAdapter extends InstructionAdapter {
 		super.putfield(owner, name, desc);
 	}
 
-	private boolean isDisposableClass(String candidate) {
+	private boolean isDisposableExternalClass(String candidate) {
 		if (reader.getClassName().equals(candidate)) {
 			// Do not instrument the owner of the member,
 			// as only external accesses have to be verified.
